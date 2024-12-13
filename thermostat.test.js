458cbe6f1e3ee373a7885de5cd401fa7cd0943c8
Karla Rangel <krangelhdz@gmail.com>
2024-12-13 15:21:15 +0000

    created tests for initial temp, up() and down()

---
+++ thermostat.test.js
@@ -0,0 +1,22 @@
+const Thermostat = require("./thermostat.js");
+
+// const thermostat = new Thermostat();
+
+describe("thermostat", () => {
+	it("starts with an initial temperature of 20 degrees", () => {
+		const thermostat = new Thermostat();
+		console.log(thermostat);
+
+		expect(thermostat.initialTemperature).toBe(20);
+	});
+	it("increases the temperature with an up method", () => {
+		const thermostat = new Thermostat();
+		thermostat.up();
+		expect(thermostat.initialTemperature).toBe(21);
+	});
+	it("decreases the temperature with a down method", () => {
+		const thermostat = new Thermostat();
+		thermostat.down();
+		expect(thermostat.initialTemperature).toBe(19);
+	});
+});

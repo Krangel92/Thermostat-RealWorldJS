889463a605650703c44fff996c852e4e9cc6c2ab
Karla Rangel <krangelhdz@gmail.com>
2024-12-13 15:22:53 +0000

    with TDD, I wrote the Thermostat class with the constructor, up() and down(). All working as expected, as per tests

--- thermostat.js
+++ thermostat.js
@@ -1,26 +1,35 @@
+//class Thermostat {
+// constructor() {
+// 	this.initialTemp = 20;
+// 	this.powerSavingModeOn = 25;
+// 	this.powerSavingModeOff = 32;
+// }
+// getTemperature() {
+// 	return this.initialTemp;
+// }
+// up() {
+// 	this.initialTemp += 1;
+// }
+// down() {
+// 	this.initialTemp -= 1;
+// }
+// setPowerSavingMode(x) {}
+// reset() {
+// 	return (this.initialTemp = 20);
+// }
+//}
+
 class Thermostat {
 	constructor() {
-		this.initialTemp = 20;
-		this.powerSavingModeOn = 25;
-		this.powerSavingModeOff = 32;
-	}
-
-	getTemperature() {
-		return this.initialTemp;
+		this.initialTemperature = 20;
 	}
 
 	up() {
-		this.initialTemp += 1;
+		this.initialTemperature += 1;
 	}
 
 	down() {
-		this.initialTemp -= 1;
-	}
-
-	setPowerSavingMode(x) {}
-
-	reset() {
-		return (this.initialTemp = 20);
+		this.initialTemperature -= 1;
 	}
 }
 

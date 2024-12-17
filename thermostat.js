d566596d90bcd49c7f17025cd4e1689996273f60
Karla Rangel <krangelhdz@gmail.com>
2024-12-17 17:07:44 +0000

    added notes

--- thermostat.js
+++ thermostat.js
@@ -1,3 +1,16 @@
+// // Exercise (instructions):
+// Build, test-driving your code, a simple program to manage a thermostat. Here is the specification for the initial MVP
+// of the program:
+
+// 	The Thermostat starts with an initial temperature of 20 degrees
+// 	You can increase the temperature with an up method
+// 	You can decrease the temperature with a down method
+// 	The minimum possible temperature is 10 degrees
+// 	The Power saving mode is on by default but it can also be turned off
+// 	If power saving mode is on, the maximum temperature is 25 degrees
+// 	If power saving mode is off, the maximum temperature is 32 degrees
+// 	You can reset the temperature to 20 with a reset method
+
 class Thermostat {
 	constructor() {
 		this.initialTemperature = 20;
@@ -41,3 +54,34 @@ class Thermostat {
 }
 
 module.exports = Thermostat;
+
+// In the node REPL:
+
+// 		> const Thermostat = require('./thermostat.js');
+// 		 undefined
+// 		> const thermostat = new Thermostat()
+// 		 undefined
+// 		> thermostat
+// 		 Thermostat {
+//   		initialTemperature: 20,
+//   		minTemperature: 10,
+//   		powerSavingMode: true,
+//   		maxPSMonTemperature: 25,
+//   		maxPSMoffTemperature: 32
+// 		 }
+// 		> thermostat.getTemperature()
+// 		 20
+// 		> thermostat.up()
+// 		 undefined
+// 		> thermostat.up()
+// 		 undefined
+// 		> thermostat.getTemperature()
+// 		 22
+// 		> thermostat.down()
+// 		 undefined
+// 		> thermostat.getTemperature()
+// 		 21
+// 		> thermostat.reset()
+// 		 undefined
+// 		> thermostat.getTemperature()
+// 		 20
